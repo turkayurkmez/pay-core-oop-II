f05446a9512ad27a82d05a90bb3f779702294537
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-06-09 11:07:44 +0300

    initialize OOP Overview

---
+++ OOPOverview/ObjectAndClass/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace ObjectAndClass
+{
+    public class Employee
+    {
+        public string Name;
+        public string LastName;
+    }
+}
---
+++ OOPOverview/ObjectAndClass/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ObjectAndClass;
+
+Employee e1 = new Employee();
+Employee e2 = new Employee();
+
+e1.Name = "Türkay";
+Employee e3 = e1;
+e3.Name = "Fikret";
+
+Console.WriteLine(e1.Name);
+
+
+Console.WriteLine("Hello, World!");
+
