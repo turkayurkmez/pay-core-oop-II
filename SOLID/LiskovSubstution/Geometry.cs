2848b8a36662953ba66d3d4a479041de25e240f7
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-06-12 10:33:28 +0300

    LSP Explained

---
+++ SOLID/LiskovSubstution/Geometry.cs
@@ -0,0 +1,33 @@
+﻿namespace LiskovSubstution
+{
+    public static class GeometryFactory
+    {
+        public static IAreaCalcutable CreateRectangle(double unitLength1, double? unitLength2 = null)
+        {
+            /*Bir biçimde kare döndürmeniz gerekti!!!!*/
+
+            return unitLength2.HasValue ? new Rectangle { Width = unitLength1, Height = unitLength2.Value } :
+                                          new Square { EdgeLength = unitLength1 };
+        }
+    }
+
+
+    public interface IAreaCalcutable
+    {
+        double GetArea();
+    }
+    public class Rectangle : IAreaCalcutable
+    {
+        public virtual double Width { get; set; }
+        public virtual double Height { get; set; }
+        public double GetArea() => Width * Height;
+
+    }
+
+    public class Square : IAreaCalcutable //: Rectangle
+    {
+        public double EdgeLength { get; set; }
+        public double GetArea() => EdgeLength * EdgeLength;
+
+    }
+}
---
+++ SOLID/LiskovSubstution/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LiskovSubstution;
+
+
+Console.WriteLine("Hello, World!");
+/*
+ * Bir base class ile derived class; birbirleriyle (herhangi bir özel kod yazmadan) yer değiştirebilir olmalı.
+ * 
+ * Derived class, base class'ın davranışlarını değiştiremez!
+ * 
+ * Bu prensip; doğru inheritance ilişkisi kurmanız içindir.
+ */
+
+Rectangle rectangle = new Rectangle() { Width = 5, Height = 10 };
+//Square square = new Square { Width = 10 };
+
+//var rect = GeometryFactory.CreateRectangle();
+//rect.Width = 10;
+//rect.Height = 6;
+//Console.WriteLine(rect.GetArea());
+
+var square = GeometryFactory.CreateRectangle(30);
+Console.WriteLine(square.GetArea());
+
+
