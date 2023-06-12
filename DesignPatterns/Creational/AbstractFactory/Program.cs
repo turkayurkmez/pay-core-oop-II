d977388a127d9063a1f77ce37419fb5aafc4fac9
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-06-12 15:19:07 +0300

    folder structure has changed

---
+++ DesignPatterns/Creational/AbstractFactory/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/*
+ * Problem:
+ * 
+ * Geliştireceğiniz uygulama; çizimi yapılan bir arayüzü, seçilen platforma göre oluşturacak.
+ *    - Bir kullanıcı giriş ekranı çizdiniz ve IOS için oluşturmasını istediniz. 
+ *    - Login ekranındali textbox, buton ve label gibi kontrolleri IOS'da oluşturması gerekiyor.
+ *    
+ *    
+ *  Senaryo:
+ *  
+ *  Bir harita uygulamasında; turisti, coğrafi ya da uydu haritası isteyebilir.... 
+ *  
+ */
