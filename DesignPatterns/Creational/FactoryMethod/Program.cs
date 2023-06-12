4484c8d58d36869be8b8b79d5eb940aa32bfea8b
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-06-12 15:09:09 +0300

    Factory explained

---
+++ DesignPatterns/Creational/FactoryMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FactoryMethod;
+
+Console.WriteLine("Hello, World!");
+/*
+ * Bir harita uygulaması geliştirmeye karar verdiniz.
+ *   Turist, gezmek istediği alanda, görmek istediği kategoriyi belirtir (Kültür, Dini, Eğlence gibi). 
+ *   Harita turistin yaptğı seçime göre api'den yerleri çeker ve haritada gösterir...
+ *   
+ *   Harita eglence = new Harita(new Eglence());
+ *   harita.ZiyaretNoktalari => Burası dolu olacak.
+ *   
+ *   Eğer senaryo kargo olsaydı.... 
+ *   
+ *           Kargo 
+ *           
+ *   KaraYoluKargo karayoluLojistik = new KaraYoluKargo
+ *   karayoluLojistik.Arac => Kamyon
+ *   
+ *   DenizYoluKargo denizyoluLojistik = new DenizYoluKargo
+ *   denizyoluLojistik.Arac => Gemi
+ *   
+ */
+EntertainmentMap entertainmentMap = new EntertainmentMap();
+foreach (var visit in entertainmentMap.VisitPoints)
+{
+    Console.WriteLine(visit.Name);
+}
+
---
+++ DesignPatterns/FactoryMethod/DP.cs
@@ -0,0 +1,83 @@
+﻿namespace FactoryMethod
+{
+    public interface IVisitPoint
+    {
+        public string Name { get; set; }
+    }
+
+    public class Pub : IVisitPoint
+    {
+        public string Name { get; set; }
+    }
+
+    public class ConcertHall : IVisitPoint
+    {
+        public string Name { get; set; }
+
+    }
+
+    public class Museum : IVisitPoint
+    {
+        public string Name { get; set; }
+
+    }
+    public class Exhibition : IVisitPoint
+    {
+        public string Name { get; set; }
+
+    }
+
+
+    public abstract class Map
+    {
+        public IEnumerable<IVisitPoint> VisitPoints { get; set; }
+        public Map()
+        {
+            //VisitPoints Map'a göre değişecek!
+            createVisitPoints();
+        }
+
+        //Bu metot sayesinde; haritada gösterilecek yerler üretiliyor:
+        //FatoryMethod:
+        protected abstract void createVisitPoints();
+
+    }
+
+    public class EntertainmentMap : Map
+    {
+        protected override void createVisitPoints()
+        {
+            VisitPoints = new List<IVisitPoint>()
+            {
+           new Pub{ Name="Teras Pub"},
+           new ConcertHall{ Name="Küçükçiftlik"},
+           new Pub{ Name="Mavi bar" }
+              };
+
+
+
+
+        }
+    }
+
+    public class CultureMap : Map
+    {
+        protected override void createVisitPoints()
+        {
+            VisitPoints = new List<IVisitPoint>()
+            {
+                  new Museum{ Name="Topkapı Saray"},
+                  new Exhibition{ Name="Akbank sanat"},
+                  new Museum{ Name ="Yerebatan sarnıcı"}
+            };
+        }
+    }
+
+    public class PoliticalMap : Map
+    {
+        protected override void createVisitPoints()
+        {
+            //Politik haritada (meclis, anıtkabir....) gibi ziyaret noktaları oluşturulacak.
+        }
+    }
+}
